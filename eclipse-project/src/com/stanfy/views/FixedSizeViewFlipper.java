966503a9e02d2c3efae7a2b5202670591ee775de
Roman Mazur <rmazur@stanfy.com.ua>
2011-03-21 15:48:38 +0000

    Fixed size view flipper

---
+++ eclipse-project/src/com/stanfy/views/FixedSizeViewFlipper.java
@@ -0,0 +1,35 @@
+package com.stanfy.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Views flipper that fixes its size according to the first view.
+ * @author Roman Mazur - Stanfy (http://www.stanfy.com)
+ */
+public class FixedSizeViewFlipper extends SafeViewFlipper {
+
+  public FixedSizeViewFlipper(final Context context) {
+    super(context);
+  }
+
+  public FixedSizeViewFlipper(final Context context, final AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override
+  protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+    final int childCount = getChildCount();
+    if (childCount < 2) {
+      super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+      return;
+    }
+    final View mainChild = getChildAt(0);
+    measureChildWithMargins(mainChild, widthMeasureSpec, 0, heightMeasureSpec, 0);
+    final int mw = mainChild.getMeasuredWidth(), mh = mainChild.getMeasuredHeight();
+    measureChildren(MeasureSpec.makeMeasureSpec(mw, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(mh, MeasureSpec.EXACTLY));
+    setMeasuredDimension(mw, mh);
+  }
+
+}
