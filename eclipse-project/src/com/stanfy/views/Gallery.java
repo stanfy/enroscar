c2e0b20d076e8a3aa687c41ca0e21f7785a8d8a3
Roman Mazur <rmazur@stanfy.com.ua>
2011-04-27 07:56:10 +0000

    Trying to upper finger behavior

--- eclipse-project/src/com/stanfy/views/Gallery.java
+++ eclipse-project/src/com/stanfy/views/Gallery.java
@@ -2,7 +2,9 @@ package com.stanfy.views;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 /**
  * @author Roman Mazur - Stanfy (http://www.stanfy.com)
@@ -11,6 +13,9 @@ public class Gallery extends android.widget.Gallery implements Runnable {
 
   /** Fling mode. */
   private boolean flingMode = false;
+
+  /** Last selected position. */
+  private int lastSelectedPosition = -1;
 
   public Gallery(final Context context) {
     this(context, null);
@@ -29,15 +34,17 @@ public class Gallery extends android.widget.Gallery implements Runnable {
     flingMode = false;
     final boolean consumed = super.onTouchEvent(event);
     final int action = event.getAction();
-    if (action == MotionEvent.ACTION_UP && !flingMode) {
+    final int currentPosition = getSelectedItemPosition();
+    Log.d(VIEW_LOG_TAG, "lastSelectedPosition=" + lastSelectedPosition + ", currentPosiotin=" + currentPosition);
+    if (action == MotionEvent.ACTION_UP && !flingMode && lastSelectedPosition != currentPosition) {
       // lifted finger action
-      final long safeDelay = 100;
+      lastSelectedPosition = currentPosition;
+      Log.d(VIEW_LOG_TAG, "lastSelectedPosition=" + lastSelectedPosition);
+      final long safeDelay = 50;
       postDelayed(this, safeDelay);
     }
     return consumed;
   }
-
-
 
   @Override
   public boolean onFling(final MotionEvent e1, final MotionEvent e2, final float velocityX, final float velocityY) {
@@ -46,8 +53,17 @@ public class Gallery extends android.widget.Gallery implements Runnable {
   }
 
   @Override
+  public boolean performItemClick(final View view, final int position, final long id) {
+    lastSelectedPosition = -1;
+    return super.performItemClick(view, position, id);
+  }
+
+  @Override
   public final void run() {
-    performItemClick(null, getSelectedItemPosition(), getSelectedItemId());
+    final int current = getSelectedItemPosition();
+    if (lastSelectedPosition == current) {
+      performItemClick(null, current, getSelectedItemId());
+    }
   }
 
 }
