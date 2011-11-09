ce13930d9f7102ba7b1955dce695b6d869783c65
Roman Mazur <rmazur@stanfy.com.ua>
2011-11-09 08:16:28 +0000

    Horizontal scroll view

--- eclipse-project/src/com/stanfy/views/HorizontalScrollView.java
+++ eclipse-project/src/com/stanfy/views/HorizontalScrollView.java
@@ -8,24 +8,24 @@ import android.util.AttributeSet;
 import android.view.View;
 
 /**
- * Scroll view that can save its position.
+ * Horizontal scroll view that can save its scroll position.
  * @author Roman Mazur (Stanfy - http://www.stanfy.com)
  */
-public class ScrollView extends android.widget.ScrollView {
+public class HorizontalScrollView extends android.widget.HorizontalScrollView {
 
   /** Froze scroll position flag. */
   private boolean frozeScrollPosition;
 
-  public ScrollView(final Context context) {
+  public HorizontalScrollView(final Context context) {
     this(context, null);
   }
 
-  public ScrollView(final Context context, final AttributeSet attrs) {
+  public HorizontalScrollView(final Context context, final AttributeSet attrs) {
     super(context, attrs);
     init(context, attrs);
   }
 
-  public ScrollView(final Context context, final AttributeSet attrs, final int defStyle) {
+  public HorizontalScrollView(final Context context, final AttributeSet attrs, final int defStyle) {
     super(context, attrs, defStyle);
     init(context, attrs);
   }
@@ -60,12 +60,12 @@ public class ScrollView extends android.widget.ScrollView {
     });
   }
 
-  public int getContentWidth() { return getWidth(); }
-  public int getContentHeight() {
-    if (getChildCount() == 0) { return getHeight(); }
+  public int getContentWidth() {
+    if (getChildCount() == 0) { return getWidth(); }
     final View child = getChildAt(0);
-    return child.getHeight();
+    return child.getWidth();
   }
+  public int getContentHeight() { return getHeight(); }
 
   /**
    * Saved state for our scroll view.
