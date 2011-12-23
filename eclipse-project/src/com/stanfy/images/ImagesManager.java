72f19984f13c0c374655d69c2678456a200ebe53
Roman Mazur <rmazur@stanfy.com.ua>
2011-12-23 18:31:43 +0000

    fix a bug with preparing memcache images

--- eclipse-project/src/com/stanfy/images/ImagesManager.java
+++ eclipse-project/src/com/stanfy/images/ImagesManager.java
@@ -660,6 +660,7 @@ public class ImagesManager<T extends CachedImage> {
       if (dstW <= 0 || dstH <= 0) { return bd; }
 
       final Bitmap scaled = Bitmap.createScaledBitmap(map, dstW, dstH, true);
+      scaled.setDensity(imagesManager.resources.getDisplayMetrics().densityDpi);
       return new BitmapDrawable(imagesManager.resources, scaled);
     }
 
