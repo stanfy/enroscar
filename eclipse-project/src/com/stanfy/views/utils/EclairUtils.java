71ea7d4af4d483fbd0ecf12fa12e9141cad28f20
Roman Mazur <rmazur@stanfy.com.ua>
2012-01-30 10:40:50 +0000

    Clean project

--- eclipse-project/src/com/stanfy/views/utils/EclairUtils.java
+++
@@ -1,9 +0,0 @@
-package com.stanfy.views.utils;
-
-/**
- * Eclair utilities (API level 7).
- * @author Roman Mazur - Stanfy (http://www.stanfy.com)
- */
-public class EclairUtils extends LowestSDKDependentUtils {
-
-}
