edf92485cd2cf5048914b5e44084ea18f08fdfc7
Roman Mazur <rmazur@stanfy.com.ua>
2012-04-11 12:21:45 +0000

    move samples from test project

--- sample-project/src/com/stanfy/enroscar/sample/SampleActivity.java
+++ sample-project/src/com/stanfy/enroscar/sample/SampleActivity.java
@@ -1,12 +1,27 @@
 package com.stanfy.enroscar.sample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.stanfy.app.activities.OneFragmentActivity;
 import com.stanfy.enroscar.sample.DevelopersLogoFragment.TweetsListener;
+import com.stanfy.enroscar.sample.other.GallerySampleActivity;
+import com.stanfy.enroscar.sample.other.QuickActionsSampleActivity;
 
-public class SampleActivity extends OneFragmentActivity<SampleApplication> implements TweetsListener {
+public class SampleActivity extends OneFragmentActivity<SampleApplication> implements TweetsListener, OnClickListener {
+
+  @Override
+  protected int getLayoutId() { return R.layout.activity; }
+
+  @Override
+  protected void onInitialize(final Bundle savedInstanceState) {
+    super.onInitialize(savedInstanceState);
+    findViewById(R.id.button_qademo).setOnClickListener(this);
+    findViewById(R.id.button_gallerydemo).setOnClickListener(this);
+  }
 
   @Override
   protected Fragment createFragment(final Bundle savedInstanceState) {
@@ -14,13 +29,23 @@ public class SampleActivity extends OneFragmentActivity<SampleApplication> imple
   }
 
   @Override
-  protected int getLayoutId() { return R.layout.activity; }
-
-  @Override
   public void onShowTweets() {
     getSupportFragmentManager().beginTransaction()
       .replace(R.id.fragment_container, new TwitterFragment())
       .commit();
+  }
+
+  @Override
+  public void onClick(final View v) {
+    switch (v.getId()) {
+    case R.id.button_qademo:
+      startActivity(new Intent(this, QuickActionsSampleActivity.class));
+      break;
+    case R.id.button_gallerydemo:
+      startActivity(new Intent(this, GallerySampleActivity.class));
+      break;
+    default:
+    }
   }
 
 }
---
+++ sample-project/src/com/stanfy/enroscar/sample/other/QuickActionsSampleActivity.java
@@ -0,0 +1,46 @@
+package com.stanfy.enroscar.sample.other;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import com.stanfy.enroscar.sample.R;
+import com.stanfy.views.qa.BasicQuickAction;
+import com.stanfy.views.qa.BasicQuickActionsAdapter;
+import com.stanfy.views.qa.QuickActionsBar;
+import com.stanfy.views.qa.QuickActionsWidget;
+import com.stanfy.views.qa.QuickActionsWidget.OnQuickActionClickListener;
+
+/**
+ * Sample for quick actions.
+ * @author Roman Mazur (Stanfy - http://www.stanfy.com)
+ */
+public class QuickActionsSampleActivity extends Activity {
+
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.qa);
+    final BasicQuickActionsAdapter adapter = new BasicQuickActionsAdapter(this,
+        new BasicQuickAction(this, R.drawable.ic_launcher, R.string.qademo_action1),
+        new BasicQuickAction(this, R.drawable.ic_launcher, R.string.qademo_action2)
+    );
+    final QuickActionsBar qaBar = new QuickActionsBar(this);
+    qaBar.setQuickActionsAdapter(adapter);
+    findViewById(R.id.qa_button).setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(final View v) {
+        qaBar.show(v);
+      }
+    });
+    qaBar.setOnQuickActionClickListener(new OnQuickActionClickListener() {
+      @Override
+      public void onQuickActionClicked(final QuickActionsWidget widget, final int position) {
+        Log.d("EnroscarQA", "Pos " + position);
+      }
+    });
+  }
+
+}
