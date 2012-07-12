547e7a56ac4b0e5492e20a5c4221e6eaa0fe72a9
Roman Mazur <rmazur@stanfy.com.ua>
2012-07-12 14:24:30 +0000

    make fiction object interface

--- eclipse-project/src/com/stanfy/content/FictionObject.java
+++ eclipse-project/src/com/stanfy/content/FictionObject.java
@@ -4,12 +4,9 @@ package com.stanfy.content;
  * Fiction object. Often it's inserted by analyzers to original lists in order to introduce list sections.
  * @author Roman Mazur (Stanfy - http://www.stanfy.com)
  */
-public abstract class FictionObject implements UniqueObject {
+public interface FictionObject extends UniqueObject {
 
   /** @return display name */
-  public abstract String getDisplayName();
-
-  @Override
-  public long getId() { return -1; }
+  String getDisplayName();
 
 }
