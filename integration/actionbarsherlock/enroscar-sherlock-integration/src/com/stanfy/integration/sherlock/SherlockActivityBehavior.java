9820fe316c2b1d755197561c261df330d123098f
Roman Mazur <rmazur@stanfy.com.ua>
2012-08-01 12:26:05 +0000

    + getSupportActionBar method

--- integration/actionbarsherlock/enroscar-sherlock-integration/src/com/stanfy/integration/sherlock/SherlockActivityBehavior.java
+++ integration/actionbarsherlock/enroscar-sherlock-integration/src/com/stanfy/integration/sherlock/SherlockActivityBehavior.java
@@ -2,6 +2,12 @@ package com.stanfy.integration.sherlock;
 
 import android.app.Activity;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockExpandableListActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockListActivity;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.stanfy.app.ActivityBehaviorFactory;
@@ -16,6 +22,26 @@ public class SherlockActivityBehavior extends BaseActivityBehavior {
 
   public SherlockActivityBehavior(final Activity activity) {
     super(activity);
+  }
+
+  public static ActionBar getSupportActionBar(final Activity activity) {
+    if (activity instanceof SherlockFragmentActivity) {
+      return ((SherlockFragmentActivity)activity).getSupportActionBar();
+    }
+    if (activity instanceof SherlockActivity) {
+      return ((SherlockActivity)activity).getSupportActionBar();
+    }
+    if (activity instanceof SherlockListActivity) {
+      return ((SherlockListActivity)activity).getSupportActionBar();
+    }
+    if (activity instanceof SherlockExpandableListActivity) {
+      return ((SherlockExpandableListActivity)activity).getSupportActionBar();
+    }
+    if (activity instanceof SherlockPreferenceActivity) {
+      return ((SherlockPreferenceActivity)activity).getSupportActionBar();
+    }
+
+    throw new IllegalArgumentException("Cannot get sherlock action bar from " + activity);
   }
 
   public boolean onCreateOptionsMenu(final Menu menu) { return false; }
