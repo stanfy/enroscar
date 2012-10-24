d44cb22f680304e63409056a9edc29287c0d857e
Roman Mazur <rmazur@stanfy.com.ua>
2012-10-24 07:58:01 +0000

    change ServiceRequestPerformer API

--- eclipse-project/src/com/stanfy/utils/ServiceRequestPerformer.java
+++ eclipse-project/src/com/stanfy/utils/ServiceRequestPerformer.java
@@ -1,10 +1,12 @@
 package com.stanfy.utils;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
 import com.stanfy.app.beans.BeansManager;
 import com.stanfy.app.service.ApplicationService;
+import com.stanfy.serverapi.request.RequestBuilder;
 import com.stanfy.serverapi.request.RequestDescription;
 
 /**
@@ -26,11 +28,32 @@ public class ServiceRequestPerformer implements RequestExecutor  {
     return description.getId();
   }
 
-  public Intent constructIntent(final RequestDescription description) {
+  private Intent constructIntent(final RequestDescription description) {
     Class<?> serviceClass = BeansManager.get(context).getRemoteServerApiConfiguration().getApplicationServiceClass();
     return new Intent(context, serviceClass)
-        .setAction(ApplicationService.ACTION_SEND_REQUEST)
-        .putExtra(ApplicationService.EXTRA_REQUEST_DESCRIPTION, description);
+      .setAction(ApplicationService.ACTION_SEND_REQUEST)
+      .putExtra(ApplicationService.EXTRA_REQUEST_DESCRIPTION, description);
+  }
+
+  /**
+   * Construct a pending intent that can be used for starting request processing.
+   * Note that after this method is executed executor of the request builder is always null.
+   * @param requestBuilder request builder instance
+   * @param flags flags to pass to {@link PendingIntent#getService(Context, int, Intent, int)}
+   * @return pending intent for starting request processing
+   */
+  public PendingIntent getPendingIntent(final RequestBuilder<?> requestBuilder, final int flags) {
+    final RequestDescription[] requestDescription = new RequestDescription[1];
+    requestBuilder.setExecutor(new RequestExecutor() {
+      @Override
+      public int performRequest(final RequestDescription rd) {
+        requestDescription[0] = rd;
+        return 0;
+      }
+    });
+    requestBuilder.execute();
+    requestBuilder.setExecutor(null);
+    return PendingIntent.getService(context, 0, constructIntent(requestDescription[0]), flags);
   }
 
 }
