cbdae6ac28f00d98efffeb5d4b8120f9f015abbc
Roman Mazur <mazur.roman@gmail.com>
2013-06-03 19:03:36 +0300

    building sdk-dep

---
+++ sdk-dep/src/jvmTest/java/com/stanfy/ernoscar/sdkdep/test/SdkDepUtilsTest.java
@@ -0,0 +1,37 @@
+package com.stanfy.ernoscar.sdkdep.test;
+
+import static org.fest.assertions.api.Assertions.*;
+
+import com.stanfy.enroscar.sdkdep.SDKDependentUtilsFactory;
+import com.stanfy.enroscar.sdkdep.SdkDependentUtils;
+import com.stanfy.enroscar.sdkdep.SdkDepUtils;
+
+import com.stanfy.enroscar.beans.BeansManager;
+
+import org.junit.runner.RunWith;
+import org.junit.Before;
+import org.junit.Test;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Test for SdkDepUtils.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class SdkDepUtilsTest {
+
+  /** Instance. */
+  private SdkDependentUtils utils;
+  
+  @Before
+  public void create() {
+    BeansManager.get(Robolectric.application).edit().put(SDKDependentUtilsFactory.class).commit();
+    utils = SdkDepUtils.get(Robolectric.application);
+  }
+  
+  @Test
+  public void utilsShouldNotBeRecreated() {
+    assertThat(utils).isSameAs(SdkDepUtils.get(Robolectric.application));
+  }
+  
+}
