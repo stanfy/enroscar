4873453044043bb4404a07716077f767b98bdc91
Roman Mazur <mazur.roman@gmail.com>
2013-07-04 13:33:01 +0300

    Simplify sample application

--- sample-project/src/com/stanfy/enroscar/sample/SampleApplication.java
+++ sample-project/src/com/stanfy/enroscar/sample/SampleApplication.java
@@ -1,23 +1,8 @@
 package com.stanfy.enroscar.sample;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.stanfy.enroscar.activities.ActivityBehaviorFactory;
-import com.stanfy.enroscar.activities.CrucialGUIOperationManager;
-import com.stanfy.enroscar.beans.BeansManager;
-import com.stanfy.enroscar.images.ViewImageConsumerFactory;
-import com.stanfy.enroscar.images.ImagesManager;
-import com.stanfy.enroscar.images.cache.ImageFileCache;
-import com.stanfy.enroscar.images.cache.SupportLruImageMemoryCache;
-import com.stanfy.enroscar.io.BuffersPool;
-import com.stanfy.enroscar.net.EnroscarConnectionsEngine;
-import com.stanfy.enroscar.rest.RemoteServerApiConfiguration;
-import com.stanfy.enroscar.rest.response.handler.GsonContentHandler;
-import com.stanfy.enroscar.rest.response.handler.StringContentHandler;
-import com.stanfy.enroscar.sdkdep.SDKDependentUtilsFactory;
-import com.stanfy.enroscar.stats.EmptyStatsManager;
-import com.stanfy.enroscar.views.ImageConsumers;
+import com.stanfy.enroscar.assist.DefaultBeansManager;
 
 /**
  * Sample application.
@@ -28,33 +13,8 @@ public class SampleApplication extends Application {
   public void onCreate() {
     super.onCreate();
 
-      Log.d("Test", "Start SampleApp");
-
-
-    BeansManager manager = BeansManager.get(this);
-    manager.edit()
-           .put(SDKDependentUtilsFactory.class)
-           .put(BuffersPool.class)
-           .put(EmptyStatsManager.class)
-           .put(ActivityBehaviorFactory.class)
-           .put(ImagesManager.IMAGE_CONSUMER_FACTORY_NAME, new ImageConsumers())
-           .put(ImageFileCache.class)
-           .put(SupportLruImageMemoryCache.class)
-           .put(ImagesManager.class)
-           .put(CrucialGUIOperationManager.class)
-           .put("defaultContentHandlerName", new GsonContentHandler(this))
-           .put(RemoteServerApiConfiguration.class)
-           .commit();
-
-//            .defaults().remoteServerApi("json").commit();
-
-
-      RemoteServerApiConfiguration config = manager.getContainer().getBean(RemoteServerApiConfiguration.class);
-      config.setDefaultContentHandlerName("defaultContentHandlerName");
-
-      EnroscarConnectionsEngine.config().install(this);
-
-      Log.d("Test", "DD: " + manager.getContainer().getBean(ActivityBehaviorFactory.class));
+    DefaultBeansManager.get(this)
+        .edit().defaults().remoteServerApi("json").commit();
   }
 
 }
