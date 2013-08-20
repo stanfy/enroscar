7f10524319d4476cc669da905d88eff4c5a05c82
Roman Mazur <mazur.roman@gmail.com>
2013-08-20 14:15:46 +0300

    make strategies generic

---
+++ content/src/jvmTest/java/com/stanfy/enroscar/content/loader/test/StrategiesContentProviderTest.java
@@ -0,0 +1,70 @@
+package com.stanfy.enroscar.content.loader.test;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import com.stanfy.enroscar.content.StrategiesContentProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+/**
+ * Tests for StrategiesContentProvider.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class StrategiesContentProviderTest {
+
+  /** Provider under the test. */
+  private StrategiesContentProvider<SQLiteOpenHelper> provider;
+
+  /** Test strategy. */
+  private StrategiesContentProvider.Strategy<SQLiteOpenHelper> testStrategy;
+
+  /** Last requested URI. */
+  private Uri lastUri;
+
+  @Before
+  public void create() {
+    provider = new StrategiesContentProvider<SQLiteOpenHelper>() {
+      @Override
+      protected void onStrategyMatcherCreate(final StrategyMatcher<SQLiteOpenHelper> matcher) {
+        testStrategy = new SimpleStrategy<SQLiteOpenHelper>() {
+          @Override
+          public Cursor query(final SQLiteOpenHelper appDbManager, final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder) {
+            lastUri = uri;
+            return super.query(appDbManager, uri, projection, selection, selectionArgs, sortOrder);
+          }
+        };
+        matcher.registerStrategy("test", "test", testStrategy);
+      }
+
+      @Override
+      protected SQLiteOpenHelper getDatabaseManager(final Context context) {
+        return null;
+      }
+    };
+    provider.onCreate();
+  }
+
+  @Test
+  public void shouldCallStrategiesCreateCallbackOnCreate() {
+    assertThat(testStrategy).describedAs("onStrategyMatcherCreate is not called").isNotNull();
+  }
+
+  @Test
+  public void shouldUseStrategies() {
+    lastUri = null;
+    provider.query(Uri.parse("content://test/test"), null, null, null, null, null);
+    assertThat(lastUri).describedAs("Query is not delegated to strategy").isNotNull();
+    lastUri = null;
+    provider.query(Uri.parse("content://test/test2"), null, null, null, null, null);
+    assertThat(lastUri).describedAs("Incorrect strategy used").isNull();
+  }
+
+}
--- content/src/main/java/com/stanfy/enroscar/content/StrategiesContentProvider.java
+++ content/src/main/java/com/stanfy/enroscar/content/StrategiesContentProvider.java
@@ -17,19 +17,22 @@ import android.util.SparseArray;
  *   Override method {@code onStrategyMatcherCreate} in order to configure bindings 
  *   and {@code getDatabaseManager} in order to provide {@link SQLiteOpenHelper} instance.
  * </p>
+ *
+ * @param <T> type of used SQLiteOpenHelper
+ *
  * @author Roman Mazur (Stanfy - http://stanfy.com)
  */
-public abstract class StrategiesContentProvider extends ContentProvider {
+public abstract class StrategiesContentProvider<T extends SQLiteOpenHelper> extends ContentProvider {
 
   /** URI matcher. */
-  private StrategyMatcher strategyMatcher;
+  private StrategyMatcher<T> strategyMatcher;
 
   /** @return StrategyMatcher instance */
-  protected StrategyMatcher getStrategyMatcher() { return strategyMatcher; }
+  protected StrategyMatcher<T> getStrategyMatcher() { return strategyMatcher; }
 
   @Override
   public boolean onCreate() {
-    strategyMatcher = new StrategyMatcher(new UriMatcher(UriMatcher.NO_MATCH));
+    strategyMatcher = new StrategyMatcher<T>(new UriMatcher(UriMatcher.NO_MATCH));
     onStrategyMatcherCreate(strategyMatcher);
     return strategyMatcher.isConfigured();
   }
@@ -48,57 +51,57 @@ public abstract class StrategiesContentProvider extends ContentProvider {
    * </p>
    * @param matcher URI matcher instance
    */
-  protected abstract void onStrategyMatcherCreate(final StrategyMatcher matcher);
+  protected abstract void onStrategyMatcherCreate(final StrategyMatcher<T> matcher);
 
   /**
    * @param context application context
    * @return database manager instance
    */
-  protected abstract SQLiteOpenHelper getDatabaseManager(final Context context);
+  protected abstract T getDatabaseManager(final Context context);
   
   @Override
   public Cursor query(final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder) {
-    final Strategy strategy = strategyMatcher.choose(uri);
+    final Strategy<T> strategy = strategyMatcher.choose(uri);
     return strategy != null ? strategy.query(getDatabaseManager(getContext()), uri, projection, selection, selectionArgs, sortOrder) : null;
   }
 
   @Override
   public String getType(final Uri uri) {
-    final Strategy strategy = strategyMatcher.choose(uri);
+    final Strategy<T> strategy = strategyMatcher.choose(uri);
     return strategy != null ? strategy.getType(getDatabaseManager(getContext()), uri) : null;
   }
 
   @Override
   public Uri insert(final Uri uri, final ContentValues values) {
-    final Strategy strategy = strategyMatcher.choose(uri);
+    final Strategy<T> strategy = strategyMatcher.choose(uri);
     return strategy != null ? strategy.insert(getDatabaseManager(getContext()), uri, values) : null;
   }
 
   @Override
   public int delete(final Uri uri, final String selection, final String[] selectionArgs) {
-    final Strategy strategy = strategyMatcher.choose(uri);
+    final Strategy<T> strategy = strategyMatcher.choose(uri);
     return strategy != null ? strategy.delete(getDatabaseManager(getContext()), uri, selection, selectionArgs) : 0;
   }
 
   @Override
   public int update(final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs) {
-    final Strategy strategy = strategyMatcher.choose(uri);
+    final Strategy<T> strategy = strategyMatcher.choose(uri);
     return strategy != null ? strategy.update(getDatabaseManager(getContext()), uri, values, selection, selectionArgs) : 0;
   }
 
   /**
    * A utility class that allows to register different content provider strategies for different URLs.
-   * @author Roman Mazur (Stanfy - http://stanfy.com)
    * @see android.content.UriMatcher
+   * @param <T> type of used SQLiteOpenHelper
    */
-  public static class StrategyMatcher {
+  public static class StrategyMatcher<T extends SQLiteOpenHelper> {
     /** Worker instance. */
     private final UriMatcher idsMatcher;
     /** Strategies register. */
-    private final SparseArray<Strategy> strategiesRegister = new SparseArray<Strategy>(10);
+    private final SparseArray<Strategy<T>> strategiesRegister = new SparseArray<Strategy<T>>(10);
 
     /** Default strategy. */
-    private Strategy defaultStrategy;
+    private Strategy<T> defaultStrategy;
 
     /** Counter. */
     private int counter = 0;
@@ -107,17 +110,17 @@ public abstract class StrategiesContentProvider extends ContentProvider {
       this.idsMatcher = idsMatcher;
     }
 
-    public void setDefaultStrategy(final Strategy defaultStrategy) {
+    public void setDefaultStrategy(final Strategy<T> defaultStrategy) {
       this.defaultStrategy = defaultStrategy;
     }
 
-    public void registerStrategy(final String authority, final String path, final Strategy strategy) {
+    public void registerStrategy(final String authority, final String path, final Strategy<T> strategy) {
       ++counter;
       idsMatcher.addURI(authority, path, counter);
       strategiesRegister.put(counter, strategy);
     }
 
-    public Strategy choose(final Uri uri) {
+    public Strategy<T> choose(final Uri uri) {
       final int id = idsMatcher.match(uri);
       if (id == UriMatcher.NO_MATCH) { return defaultStrategy; }
       return strategiesRegister.get(id, defaultStrategy);
@@ -126,59 +129,65 @@ public abstract class StrategiesContentProvider extends ContentProvider {
     public boolean isConfigured() { return counter > 0 || defaultStrategy != null; }
   }
 
-  /** Content provider strategy. */
-  public interface Strategy {
+  /**
+   * Content provider strategy.
+   * @param <T> type of SQLiteOpenHelper
+   */
+  public interface Strategy<T extends SQLiteOpenHelper> {
     /**
      * @see android.content.ContentProvider#query(Uri, String[], String, String[], String)
      */
-    Cursor query(final SQLiteOpenHelper appDbManager, final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder);
+    Cursor query(final T appDbManager, final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder);
 
     /**
      * @see android.content.ContentProvider#getType(Uri)
      */
-    String getType(final SQLiteOpenHelper appDbManager, final Uri uri);
+    String getType(final T appDbManager, final Uri uri);
 
     /**
      * @see android.content.ContentProvider#insert(Uri, ContentValues)
      */
-    Uri insert(final SQLiteOpenHelper appDbManager, final Uri uri, final ContentValues values);
+    Uri insert(final T appDbManager, final Uri uri, final ContentValues values);
 
     /**
      * @see android.content.ContentProvider#delete(Uri, String, String[])
      */
-    int delete(final SQLiteOpenHelper appDbManager, final Uri uri, final String selection, final String[] selectionArgs);
+    int delete(final T appDbManager, final Uri uri, final String selection, final String[] selectionArgs);
 
     /**
      * @see android.content.ContentProvider#update(Uri, ContentValues, String, String[])
      */
-    int update(final SQLiteOpenHelper appDbManager, final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs);
+    int update(final T appDbManager, final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs);
   }
 
-  /** Simple strategy, use it when you do not want to implement all the methods of {@link Strategy}. */
-  public static class SimpleStrategy implements Strategy {
+  /**
+   * Simple strategy, use it when you do not want to implement all the methods of {@link Strategy}.
+   * @param <T> type of SQLiteOpenHelper
+   */
+  public static class SimpleStrategy<T extends SQLiteOpenHelper> implements Strategy<T> {
 
     @Override
-    public Cursor query(final SQLiteOpenHelper appDbManager, final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder) {
+    public Cursor query(final T appDbManager, final Uri uri, final String[] projection, final String selection, final String[] selectionArgs, final String sortOrder) {
       return null;
     }
 
     @Override
-    public String getType(final SQLiteOpenHelper appDbManager, final Uri uri) {
+    public String getType(final T appDbManager, final Uri uri) {
       return null;
     }
 
     @Override
-    public Uri insert(final SQLiteOpenHelper appDbManager, final Uri uri, final ContentValues values) {
+    public Uri insert(final T appDbManager, final Uri uri, final ContentValues values) {
       return null;
     }
 
     @Override
-    public int delete(final SQLiteOpenHelper appDbManager, final Uri uri, final String selection, final String[] selectionArgs) {
+    public int delete(final T appDbManager, final Uri uri, final String selection, final String[] selectionArgs) {
       return 0;
     }
 
     @Override
-    public int update(final SQLiteOpenHelper appDbManager, final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs) {
+    public int update(final T appDbManager, final Uri uri, final ContentValues values, final String selection, final String[] selectionArgs) {
       return 0;
     }
 
