b4e2d3d871d491be9700a72976067a91549ce69d
Roman Mazur <rmazur@stanfy.com.ua>
2014-05-30 14:54:07 +0300

    update plugin and build tools version

--- scripts/install_android_sdk.sh
+++ scripts/install_android_sdk.sh
@@ -3,5 +3,5 @@
 sudo apt-get update -q
 sudo apt-get install -qq libstdc++6:i386 lib32z1 expect
 
-COMPONENTS=build-tools-19.0.3,extra-android-support,android-19,extra-android-m2repository,sysimg-19
+COMPONENTS=build-tools-19.1.0,extra-android-support,android-19,extra-android-m2repository,sysimg-19
 curl -L https://raw.github.com/embarkmobile/android-sdk-installer/version-2/android-sdk-installer | bash /dev/stdin --install=$COMPONENTS
