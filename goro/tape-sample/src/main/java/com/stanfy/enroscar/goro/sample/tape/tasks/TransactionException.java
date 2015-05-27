f61d149251e584b1876b8f50cc0f50a5ce7a37f4
Oleksandr Tereshchuk <rivne2@gmail.com>
2015-05-27 20:12:27 +0300

    Goro moved to separate repo

--- goro/tape-sample/src/main/java/com/stanfy/enroscar/goro/sample/tape/tasks/TransactionException.java
+++
@@ -1,7 +0,0 @@
-package com.stanfy.enroscar.goro.sample.tape.tasks;
-
-/**
- * Thrown when {@link com.stanfy.enroscar.goro.sample.tape.tasks.TransactionTask} fails.
- */
-public class TransactionException extends RuntimeException {
-}
